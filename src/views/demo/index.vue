68b7d8e9deb7960ec9484b79c8f01049275a2d8e
MDT <2463619061@qq.com>
2022-07-24 19:28:31 +0800

    feat(robot):聊天机器人_聊天消息自动滚动至可视窗口

---
+++ src/views/demo/index.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Description : DEMO 页面
+ * @Author      : huazf
+ * @Date        : 2022-07-24
+ * @LastEditors : huazf
+ * @LastEditTime: 2022-07-24
+ * @FilePath    : \vue3-demo\src\views\demo\index.vue
+-->
+<script setup name="Demo">
+import { ref, reactive } from 'vue'
+
+</script>
+
+<template>
+  <div class="">
+    demo
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
