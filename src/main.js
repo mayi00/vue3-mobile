6a75979a3a554906d67e0f00f93d374d59b2cc27
MDT <2463619061@qq.com>
2024-08-12 10:35:05 +0800

    VueSignaturePad

--- src/main.js
+++ src/main.js
@@ -8,11 +8,14 @@ import store from './store'
 // rem 布局适配
 import 'amfe-flexible'
 // import VConsole from 'vconsole'
+import VueSignaturePad from 'vue-signature-pad'
+
 const app = createApp(App)
 console.log('process.env:', process.env)
 console.info('import.meta.env:', import.meta.env)
 
 // app.use(VConsole)
+app.use(VueSignaturePad)
 app.use(router)
 app.use(store)
 app.mount('#app')
